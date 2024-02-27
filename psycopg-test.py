beb560b7eaece9b8e229ddc6f8b0b977ab49aa0e
Alan Zhou <54216387+AlanReviews@users.noreply.github.com>
2024-02-26 20:21:49 -0500

    Boilerplate code
    Added Python code to experiment with how the program works

---
+++ psycopg-test.py
@@ -0,0 +1,21 @@
+import psycopg, csv
+
+def main():
+    try:
+        connection = psycopg.connect(
+            "dbname=University user=postgres "
+            "password=ab12345 host=localhost port=5432"
+        )
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM instructor;")
+            with open('test.csv', 'w', newline='\n') as out:
+                csv_out=csv.writer(out)
+                data = [cursor.fetchall()]
+                for r in data:
+                    csv_out.writerow(r)
+
+    except psycopg.OperationalError as e:
+        print(f"error: {e}")
+        exit(1)
+
+main()

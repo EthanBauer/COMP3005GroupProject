272db446f4bb20a75b2dadaccebb8d8c9c23c14a
men9xuan <44915783+men9xuan@users.noreply.github.com>
2024-04-03 13:06:45 -0400

    fix

--- event_pass.sql
+++ event_pass.sql
@@ -1,6 +1,6 @@
 CREATE TABLE pass_events (
 	event_id VARCHAR(50) PRIMARY KEY,
-	team VARCHAR(20),
+	team VARCHAR(40),
 	team_id SMALLINT,
 	player VARCHAR(40),
 	player_id INT,
@@ -8,8 +8,10 @@ CREATE TABLE pass_events (
 	recipient_id INT,
 	type VARCHAR(16),
 	type_id SMALLINT,
+	technique VARCHAR(20),
+	technique_id SMALLINT,
 	end_loc_x numeric(5,2),
 	end_loc_y numeric(5,2),
 	outcome_id VARCHAR(3),
-	outcome VARCHAR(20)
+	outcome VARCHAR(40)
 );

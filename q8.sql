97f67d4e8c64a53fdbec324a947a4c1272db6ee1
men9xuan <44915783+men9xuan@users.noreply.github.com>
2024-04-04 12:20:05 -0400

    Update q8.sql

--- q8.sql
+++ q8.sql
@@ -1,8 +1,8 @@
-SELECT team, COUNT(pass_events.event_id) AS through_ball_count
-FROM pass_events 
-JOIN events ON pass_events.event_id = events.event_id
+SELECT team, COUNT(events_pass.event_id) AS through_ball_count
+FROM events_pass 
+JOIN events ON events_pass.event_id = events.event_id
 JOIN matches ON events.match_id = matches.match_id
 WHERE season = '2020/2021' AND competition_id = 11 AND technique_id = 108
 GROUP BY team
-HAVING COUNT(pass_events.event_id) > 0
+HAVING COUNT(events_pass.event_id) > 0
 ORDER BY through_ball_count DESC;

b9a9a49ca49ccc89d859ee37f0e294acb120c294
Ethan Bauer <ethanbauer@cmail.carleton.ca>
2024-04-04 17:53:24 -0400

    hotfix q1.sql

--- q1.sql
+++ q1.sql
@@ -3,6 +3,6 @@ FROM events_shot
 JOIN events ON events_shot.event_id = events.event_id
 JOIN matches ON events.match_id = matches.match_id
 WHERE season = '2020/2021' AND competition = 'La Liga'
-GROUP BY player
+GROUP BY player, player_id
 HAVING AVG(xg_score) > 0
 ORDER BY AVG(xg_score) DESC;

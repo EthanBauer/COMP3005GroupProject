c4df9623ab2182732b35015134f4cb96eb923503
Ethan Bauer <ethanbauer@cmail.carleton.ca>
2024-04-05 18:11:35 -0400

    q4 hotfix

--- q4.sql
+++ q4.sql
@@ -2,7 +2,7 @@ SELECT team, COUNT(*) AS num_passes
 FROM events_pass
 JOIN events ON events_pass.event_id = events.event_id
 JOIN matches ON events.match_id = matches.match_id
-WHERE season = '2020/2021'
+WHERE season = '2020/2021' AND competition_id = 11 
 GROUP BY team
 HAVING COUNT(*) >= 1
 ORDER BY num_passes DESC

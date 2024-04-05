0ffc5ab4b5d4ba691c26b8ef40c5e563e735ca96
men9xuan <44915783+men9xuan@users.noreply.github.com>
2024-04-05 00:05:38 -0400

    Update q2.sql

--- q2.sql
+++ q2.sql
@@ -5,4 +5,4 @@ JOIN matches ON events.match_id = matches.match_id
 WHERE season = '2020/2021'
 GROUP BY player, player_id
 HAVING COUNT(*) >= 1
-ORDER BY num_shots DESC
+ORDER BY num_shots DESC, player ASC 

ab6821ad5176e17fd087ffebf3e22e1078a6a402
men9xuan <44915783+men9xuan@users.noreply.github.com>
2024-04-06 16:55:23 -0400

    optimize query
    use competition_id for query is slightly faster

--- q1.sql
+++ q1.sql
@@ -2,7 +2,8 @@ SELECT player, AVG(xg_score) AS average_xg_score
 FROM events_shot 
 JOIN events ON events_shot.event_id = events.event_id
 JOIN matches ON events.match_id = matches.match_id
-WHERE season = '2020/2021' AND competition = 'La Liga'
+-- WHERE season = '2020/2021' AND competition = 'La Liga'
+WHERE season = '2020/2021' AND competition_id = 11
 GROUP BY player, player_id
 HAVING AVG(xg_score) > 0
 ORDER BY AVG(xg_score) DESC;
--- q2.sql
+++ q2.sql
@@ -2,7 +2,8 @@ SELECT player, COUNT(*) AS num_shots
 FROM events_shot
 JOIN events ON events_shot.event_id = events.event_id
 JOIN matches ON events.match_id = matches.match_id
-WHERE season = '2020/2021' AND competition = 'La Liga'
+-- WHERE season = '2020/2021' AND competition = 'La Liga'
+WHERE season = '2020/2021' AND competition_id = 11
 GROUP BY player, player_id
 HAVING COUNT(*) >= 1
 ORDER BY num_shots DESC, player ASC 

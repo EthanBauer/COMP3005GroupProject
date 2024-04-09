6fa8e96389c4b7b843d44cf9572d4da5e0e80a30
Ethan Bauer <ethanbauer@cmail.carleton.ca>
2024-04-09 00:01:44 -0400

    q5 q6

---
+++ q6.sql
@@ -0,0 +1,8 @@
+SELECT team,COUNT(events_shot.event_id) AS team_shot_count
+FROM events_shot
+JOIN events ON events_shot.event_id = events.event_id
+JOIN matches ON events.match_id = matches.match_id
+WHERE matches.season = '2003/2004' AND matches.competition_id = 2
+GROUP BY events_shot.team, events_shot.team_id
+HAVING COUNT(events_shot.event_id) > 0
+ORDER BY team_shot_count DESC;

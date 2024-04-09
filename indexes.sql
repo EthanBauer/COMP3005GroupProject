4c054a3ea9b31d97c97a2ca4a8362e0dd67400a8
Ethan Bauer <ethanbauer@cmail.carleton.ca>
2024-04-09 13:43:48 -0400

    indexes

---
+++ indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX IF NOT EXISTS events_shot_index ON events_shot (event_id);
+CREATE INDEX IF NOT EXISTS events_pass_index ON events_pass (event_id);
+CREATE INDEX IF NOT EXISTS events_index ON events (match_id);
+CREATE INDEX IF NOT EXISTS matches_index ON matches (season_id, competition_id);
+CREATE INDEX IF NOT EXISTS players_index ON players (player_id);

14345a7e31491ca270ff34c8f766a5e228b5ce95
men9xuan <44915783+men9xuan@users.noreply.github.com>
2024-04-11 21:47:55 -0400

    update q6 and add new q5

---
+++ q5 new.sql
@@ -0,0 +1,8 @@
+SELECT recipient ,COUNT(events_pass.event_id) AS intended_recipient_count
+FROM events_pass
+JOIN events ON events_pass.event_id = events.event_id
+JOIN matches ON events.match_id = matches.match_id
+WHERE matches.season_id = 44 AND matches.competition_id = 2
+GROUP BY recipient_id, recipient
+HAVING COUNT(events_pass.event_id) > 0
+ORDER BY intended_recipient_count DESC;
--- q6.sql
+++ q6.sql
@@ -3,6 +3,6 @@ FROM events_shot
 JOIN events ON events_shot.event_id = events.event_id
 JOIN matches ON events.match_id = matches.match_id
 WHERE matches.season_id = 44 AND matches.competition_id = 2
-GROUP BY events_shot.team, events_shot.team_id
-HAVING COUNT(events_shot.event_id) > 0
+GROUP BY events.team, events.team_id
+HAVING COUNT(events.event_id) > 0
 ORDER BY team_shot_count DESC;

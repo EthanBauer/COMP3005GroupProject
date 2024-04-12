0f52eaecabc0710efd2342e268e214f3d919a93c
men9xuan <44915783+men9xuan@users.noreply.github.com>
2024-04-12 18:11:19 -0400

    add dbexport

--- indexes.sql
+++ indexes.sql
@@ -2,5 +2,8 @@ CREATE INDEX IF NOT EXISTS events_shot_index ON events_shot (event_id);
 CREATE INDEX IF NOT EXISTS events_pass_index ON events_pass (event_id);
 CREATE INDEX IF NOT EXISTS events_index ON events (match_id);
 CREATE INDEX IF NOT EXISTS matches_index ON matches (season_id, competition_id);
+CREATE INDEX IF NOT EXISTS matches_index2 ON matches (season_id);
+CREATE INDEX IF NOT EXISTS matches_index3 ON matches (competition_id);
+CREATE INDEX IF NOT EXISTS matches_index4 ON matches (match_id);
 CREATE INDEX IF NOT EXISTS players_index ON players (player_id);
 CREATE INDEX IF NOT EXISTS lineups_index ON lineups (match_id, player_id, team_id);

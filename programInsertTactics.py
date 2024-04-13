26b8198ab49f41f0788a5eff898aae3a50f9df4b
men9xuan <44915783+men9xuan@users.noreply.github.com>
2024-04-13 13:00:31 -0400

    Squashed commit of the following:
    commit 9c813ac9840cd73713feb6d443d174aa98554d61
    Author: Ethan Bauer <ethanbauer@cmail.carleton.ca>
    Date:   Sat Apr 13 02:35:25 2024 -0400
    
        tactics
    
    commit 5b826ae3c73474c9de8d9dd01d9d26d0864effb5
    Author: Ethan Bauer <ethanbauer@cmail.carleton.ca>
    Date:   Sat Apr 13 01:59:37 2024 -0400
    
        shots
    
    commit 208a6bf3a1d53b7997bb5995fb2a3b8f8babc143
    Author: Ethan Bauer <ethanbauer@cmail.carleton.ca>
    Date:   Sat Apr 13 01:56:28 2024 -0400
    
        ethan pull request
    
    commit 3067f4fcfb92750b6e102b6f1937879fb0cf8b19
    Author: Ethan Bauer <ethanbauer@cmail.carleton.ca>
    Date:   Sat Apr 13 01:56:15 2024 -0400
    
        ethan pulling?

--- programInsertTactics.py
+++ programInsertTactics.py
@@ -6,10 +6,10 @@ import json
 from pathlib import Path
 
 # Define connection parameters
-dbname = 'projecttest'
+dbname = 'Project1'
 user = 'postgres'
-password = 'postgres'
-host = '127.0.0.1'  
+password = 'Hannah14!'
+host = 'localhost'  
 port = '5432'  
 
 # Helper function to connect to postgres server
@@ -33,18 +33,11 @@ def connect_db():
 # get useful match ids from the match table  
 conn = connect_db()
 cursor = conn.cursor()
-# cursor.execute("SELECT match_id FROM public.matches ORDER BY match_id ASC; ")
-# id_list = [row[0] for row in cursor.fetchall()]
-# conn.close()
 
 id_list = [15946, 15956, 15973, 15978, 15986, 15998, 16010, 16023, 16029, 16056, 16073, 16079, 16086, 16095, 16109, 16120, 16131, 16136, 16149, 16157, 16173, 16182, 16190, 16196, 16205, 16215, 16231, 16240, 16248, 16265, 16275, 16289, 16306, 16317, 303377, 303400, 303421, 303430, 303451, 303470, 303473, 303479, 303487, 303493, 303504, 303516, 303517, 303524, 303532, 303548, 303596, 303600, 303610, 303615, 303634, 303652, 303664, 303666, 303674, 303680, 303682, 303696, 303700, 303707, 303715, 303725, 303731, 3749052, 3749068, 3749079, 3749108, 3749117, 3749133, 3749153, 3749192, 3749196, 3749233, 3749246, 3749253, 3749257, 3749274, 3749276, 3749278, 3749296, 3749310, 3749346, 3749358, 3749360, 3749403, 3749431, 3749434, 3749448, 3749453, 3749454, 3749462, 3749465, 3749493, 3749522, 3749526, 3749528, 3749552, 3749590, 3749603, 3749631, 3749642, 3764440, 3764661, 3773369, 3773372, 3773377, 3773386, 3773387, 3773403, 3773415, 3773428, 3773457, 3773466, 3773474, 3773477, 3773497, 3773523, 3773526, 3773547, 3773552, 3773565, 3773571, 3773585, 3773586, 3773587, 3773593, 3773597, 3773625, 3773631, 3773656, 3773660, 3773661, 3773665, 3773672, 3773689, 3773695]
 
-# print(id_list)
-# print(len(id_list))
-
-# directory = r'C:\Users\mengx\Downloads\GithubRepo\open-data\data\events'
 directory = r'/Users/ethanbauer/Downloads/W2024/COMP3005/GithubRepo/open-data-master/data/events'
-# file_pathes = Path(directory).glob("*.json")
+
 
 filecount = 0 
 for id in id_list:
@@ -57,30 +50,28 @@ for id in id_list:
         for entry in data:
             # insert common event data to 'events' table
             # todo: add more common attributes to event table
-            print(id)
-            print(entry['id'])
+            # print(id)
+            # print(entry['id'])
+            
+            if entry.get('tactics'):
+                # import sys
+                # sys.exit()
+                tactic_data = {'event_id': entry['id'],
+                               'formation': entry['tactics']['formation'],
+                               'player_id': [],
+                               'player_name': [],
+                               'position_id': [],
+                               'position_name': [],
+                               'jersey_number': []}
+                for player in entry['tactics']['lineup']:
+                    tactic_data['player_id'].append(player['player']['id'])
+                    tactic_data['player_name'].append(player['player']['name'])
+                    tactic_data['position_id'].append(player['position']['id'])
+                    tactic_data['position_name'].append(player['position']['name'])
+                    tactic_data['jersey_number'].append(player['jersey_number'])
 
-            # shot
-            if entry['type']['id'] == 16:
-                # print(id)
-                # print(entry['id'])
-
-                secondary_event_data = {
-                    'uid': entry['id'],
-                    'xg_score': entry['shot']['statsbomb_xg'],
-                    'first_time': entry['shot']['first_time'] if entry['shot'].get('first_time') else False,
-                    'end_location': entry['shot']['end_location'] if entry['shot'].get('end_location') else None,
-                    'technique_id': entry['shot']['technique']['id'] if entry['shot'].get('technique') else None,
-                    'technique_label': entry['shot']['technique']['name'] if entry['shot'].get('technique') else None,
-                    'body_part_id': entry['shot']['body_part']['id'] if entry['shot'].get('body_part') else None,
-                    'body_part_label': entry['shot']['body_part']['name'] if entry['shot'].get('body_part') else None,
-                    'type_id': entry['shot']['type']['id'] if entry['shot'].get('type') else None,
-                    'type_label': entry['shot']['type']['name'] if entry['shot'].get('type') else None,
-                    'outcome_id': entry['shot']['outcome']['id'] if entry['shot'].get('outcome') else None,
-                    'outcome_label': entry['shot']['outcome']['name'] if entry['shot'].get('outcome') else None
-                }
-
-                # cursor.execute('''
-                #         INSERT INTO events_shot (event_id, xg_score, first_time, end_location, technique_id, technique_label, body_part_id, body_part_label, type_id, type_label, outcome_id, outcome_label)
-                #         VALUES (%(uid)s, %(xg_score)s, %(first_time)s, %(end_location)s, %(technique_id)s, %(technique_label)s, %(body_part_id)s, %(body_part_label)s, %(type_id)s, %(type_label)s, %(outcome_id)s, %(outcome_label)s)
-                #     ''', secondary_event_data) 
+                cursor.execute('''
+                        INSERT INTO tactics (event_id, formation, player_id, player_name, position_id, position_name, jersey_number)
+                        VALUES (%(event_id)s, %(formation)s, %(player_id)s, %(player_name)s, %(position_id)s, %(position_name)s, %(jersey_number)s)
+                    ''', tactic_data) 
+            

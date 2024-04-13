5839b8c1e66280d33c0edd4a84d8a9ebde76fee2
men9xuan <44915783+men9xuan@users.noreply.github.com>
2024-04-12 22:50:23 -0400

    update q5

--- subnmission/queries.py
+++ subnmission/queries.py
@@ -272,11 +272,11 @@ def Q_5(cursor, conn, execution_time):
     #==========================================================================    
     # Enter QUERY within the quotes:
     
-    query = """SELECT recipient_id, recipient ,COUNT(events_pass.event_id) AS intended_recipient_count
+    query = """SELECT recipient, COUNT(events_pass.event_id) AS intended_recipient_count
 FROM events_pass
 JOIN events ON events_pass.event_id = events.event_id
 JOIN matches ON events.match_id = matches.match_id
-WHERE matches.season_id = 44 AND matches.competition_id = 2 AND recipient IS NOT null 
+WHERE matches.season_id = 44 AND matches.competition_id = 2 AND recipient_id IS NOT null 
 GROUP BY recipient_id, recipient
 HAVING COUNT(events_pass.event_id) > 0
 ORDER BY intended_recipient_count DESC;"""

4005127bdd9747a5acdbc8a778ee072730932ecb
men9xuan <44915783+men9xuan@users.noreply.github.com>
2024-04-12 22:24:58 -0400

    update q9

--- q9.sql
+++ q9.sql
@@ -2,7 +2,7 @@ SELECT player, COUNT(events_dribble.event_id) AS completed_dribbles
 FROM events_dribble
 JOIN events ON events_dribble.event_id = events.event_id
 JOIN matches ON events.match_id = matches.match_id
-WHERE season_id in (4, 42, 90) AND competition_id = 11
+WHERE season_id in (4, 42, 90) AND competition_id = 11 AND complete = TRUE
 GROUP BY player, player_id
 HAVING COUNT(events_dribble.event_id) > 0
 ORDER BY completed_dribbles DESC;

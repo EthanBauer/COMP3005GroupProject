e7f1f4d49154f58623006e65cb1bc0cd3b7afeb0
Ethan Bauer <51236835+EthanBauer@users.noreply.github.com>
2024-04-13 17:37:21 -0400

    events_startingxi.sql

--- events_startingxi.sql
+++ events_startingxi.sql
@@ -1,16 +1,7 @@
-CREATE TABLE startingxi (
+CREATE TABLE events_startingxi (
     event_id VARCHAR(50) PRIMARY KEY,
-    period INTEGER,
-    timestamp TIME,
-    minute INTEGER,
-    second INTEGER,
+    match_id INTEGER,
     possession INTEGER,
     possession_team_id INTEGER,
-    possession_team_name VARCHAR(100),
-    play_pattern_id INTEGER,
-    play_pattern_name VARCHAR(100),
-    team_id INTEGER,
-    team_name VARCHAR(100),
-    duration FLOAT,
-    tactics_formation INTEGER
+    possession_team_name VARCHAR(100)
 );

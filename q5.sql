4e81bbb69576349b498347796577a074b74eaa86
men9xuan <44915783+men9xuan@users.noreply.github.com>
2024-04-12 22:46:56 -0400

    update q5 and queries

--- q5.sql
+++ q5.sql
@@ -1,9 +1,8 @@
-SELECT players.player_name,COUNT(events_pass.event_id) AS intended_recipient_count
+SELECT recipient ,COUNT(events_pass.event_id) AS intended_recipient_count
 FROM events_pass
 JOIN events ON events_pass.event_id = events.event_id
 JOIN matches ON events.match_id = matches.match_id
-JOIN players ON events.player_id = players.player_id
-WHERE matches.season_id = 44 AND matches.competition_id = 2
-GROUP BY players.player_id, players.player_name
+WHERE matches.season_id = 44 AND matches.competition_id = 2 AND recipient IS NOT null 
+GROUP BY recipient_id, recipient
 HAVING COUNT(events_pass.event_id) > 0
 ORDER BY intended_recipient_count DESC;

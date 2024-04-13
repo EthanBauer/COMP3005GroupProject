ed03f0a428da130510f4190d5b7c877fe62075fb
men9xuan <44915783+men9xuan@users.noreply.github.com>
2024-04-12 22:43:48 -0400

    fix q5

--- q5.sql
+++ q5.sql
@@ -2,7 +2,7 @@ SELECT players.player_name,COUNT(events_pass.event_id) AS intended_recipient_cou
 FROM events_pass
 JOIN events ON events_pass.event_id = events.event_id
 JOIN matches ON events.match_id = matches.match_id
-JOIN players ON events_pass.player_id = players.player_id
+JOIN players ON events.player_id = players.player_id
 WHERE matches.season_id = 44 AND matches.competition_id = 2
 GROUP BY players.player_id, players.player_name
 HAVING COUNT(events_pass.event_id) > 0
